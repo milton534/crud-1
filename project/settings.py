ecd132624939fc84228166d261b1c5e032091c5f
“user” <j5w9a8@bma.biglobe.ne.jp>
2021-02-01 12:16:48 +0900

    Commit

--- project/settings.py
+++ project/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django-crispy-forms',
+#    'django-crispy-forms',
+    'crispy_forms',
     'app',
 ]
 
